a35a36c40d5d3d2efbedc64e931bd5e30b4f0f0e
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-10-10 15:20:48 +0200

    Copied P2d and V2d into model

--- src/main/java/sap/ass01/solution/frontend/model/EBike.java
+++ src/main/java/sap/ass01/solution/frontend/model/EBike.java
@@ -1,13 +1,10 @@
 package sap.ass01.solution.frontend.model;
 
-import sap.ass01.bbom.P2d;
-import sap.ass01.bbom.V2d;
-
 public record EBike(
-        String id,
-        EBikeState state,
-        P2d loc,
-        V2d direction,
-        double speed,
-        int batteryLevel) {
+                String id,
+                EBikeState state,
+                P2d loc,
+                V2d direction,
+                double speed,
+                int batteryLevel) {
 }
