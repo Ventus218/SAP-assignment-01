ab4c0ad2c6dcfa21029858c686288552e95c2518
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-10-19 16:48:51 +0200

    Set HTTPAPIs impl in EBikeAdminApp

--- src/main/java/sap/ass01/solution/frontend/admin/EBikeAdminApp.java
+++ src/main/java/sap/ass01/solution/frontend/admin/EBikeAdminApp.java
@@ -2,12 +2,16 @@ package sap.ass01.solution.frontend.admin;
 
 import javax.swing.*;
 
+import io.vertx.core.json.jackson.DatabindCodec;
 import sap.ass01.solution.frontend.model.HTTPAPIs;
+import sap.ass01.solution.frontend.model.HTTPAPIsImpl;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 public class EBikeAdminApp {
 
     public static void main(String[] args) {
-        HTTPAPIs apis = null; // TODO: set implementation
+        DatabindCodec.mapper().registerModule(new Jdk8Module());
+        HTTPAPIs apis = new HTTPAPIsImpl("localhost", 8080);
         SwingUtilities.invokeLater(() -> new AdminControlPanelView(new AdminControlPanelViewModel(apis)));
     }
 }
