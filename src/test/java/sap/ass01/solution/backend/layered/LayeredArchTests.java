d549f11127d6ffbd9fccd9e4306741d8a7629700
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-10-22 11:34:49 +0200

    Refactor Layered architecture tests

--- src/test/java/sap/ass01/solution/backend/layered/LayeredArchTests.java
+++ src/test/java/sap/ass01/solution/backend/layered/LayeredArchTests.java
@@ -1,45 +1,58 @@
 package sap.ass01.solution.backend.layered;
 
-import java.util.*;
 import org.junit.jupiter.api.Test;
 import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.core.importer.*;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
 public class LayeredArchTests {
-    /**
-     * Testing that in a layered architecture each layer can depend only on the
-     * layer below (without skipping layers)
-     */
+
+    final String layered = "sap.ass01.solution.backend.layered";
+    final String presentation = layered + ".presentation";
+    final String businesslogic = layered + ".businesslogic";
+    final String persistence = layered + ".persistence";
+    final String database = layered + ".database";
+    final String java = "java";
+    final String vertx = "io.vertx";
+    final String json = vertx + ".core.json";
+
+    final JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(new ImportOption.DoNotIncludeTests())
+            .importPackages(
+                    presentation,
+                    businesslogic,
+                    persistence,
+                    database);
+
     @Test
-    public void test() {
-        String layered = "sap.ass01.solution.backend.layered";
-        String presentation = layered + ".presentation";
-        String businesslogic = layered + ".businesslogic";
-        String persistence = layered + ".persistence";
-        String database = layered + ".database";
-        JavaClasses importedClasses = new ClassFileImporter().importPackages(
-                presentation,
-                businesslogic,
-                persistence,
-                database);
+    public void PresentationShouldDependOnlyOnBusinessLogic() {
+        classes().that().resideInAPackage(presentation + "..")
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(java + "..", vertx + "..", presentation + "..", businesslogic + "..")
+                .check(importedClasses);
+    }
 
-        List<ArchRule> rules = new ArrayList<>();
+    @Test
+    public void BusinessLogicShouldDependOnlyOnPersistence() {
+        classes().that().resideInAPackage(businesslogic + "..")
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(java + "..", businesslogic + "..", persistence + "..")
+                .check(importedClasses);
+    }
 
-        rules.add(
-                classes().that().resideInAPackage(presentation + "..")
-                        .should().onlyBeAccessed().byAnyPackage(presentation + ".."));
-        rules.add(
-                classes().that().resideInAPackage(businesslogic + "..")
-                        .should().onlyBeAccessed().byAnyPackage(businesslogic + "..", presentation + ".."));
-        rules.add(
-                classes().that().resideInAPackage(persistence + "..")
-                        .should().onlyBeAccessed().byAnyPackage(persistence + "..", businesslogic + ".."));
-        rules.add(
-                classes().that().resideInAPackage(database + "..")
-                        .should().onlyBeAccessed().byAnyPackage(database + "..", persistence + ".."));
+    @Test
+    public void PersistenceShouldDependOnlyOnDatabase() {
+        classes().that().resideInAPackage(persistence + "..")
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(java + "..", json + "..", persistence + "..", database + "..")
+                .check(importedClasses);
+    }
 
-        rules.forEach(r -> r.check(importedClasses));
+    @Test
+    public void DatabaseShouldNotDependOnAnything() {
+        classes().that().resideInAPackage(database + "..")
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(java + "..", database + "..")
+                .check(importedClasses);
     }
 }
